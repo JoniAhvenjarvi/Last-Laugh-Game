ed82dfe93fb1b64bff8f5b9d117e3b1f2fc13c3d
Joni Ahvenjärvi <joniahvenjarvi@kamk.fi>
2020-06-24 14:13:12 +0300

    Toimiiko?
    Testiä

---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform PlayerPosition;
+    public Transform EnemyPosition;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public BattleState state;
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+       GameObject playerGO = Instantiate(playerPrefab, PlayerPosition);
+       playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, EnemyPosition);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        //enemyUnit.unitName
+    }
+}
